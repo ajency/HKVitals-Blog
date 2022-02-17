05d70791ad1f64ec3344bd811b309705b182984f
mrnarvekar <mahaveer@ajency.in>
2022-02-17 12:43:15 +0530

    Initial commit
    HK Vitals Blog initial commit

---
+++ wp-content/themes/hkvitals-blog-child/page-templates/home.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Genesis Sample.
+ *
+ * This file adds the landing page template to the Genesis Sample Theme.
+ *
+ * Template Name: Home
+ *
+ * @package Genesis Sample
+ * @author  StudioPress
+ * @license GPL-2.0-or-later
+ * @link    https://www.studiopress.com/
+ */
+get_header();
+?>
+
+<div class="container-main">
+	<?php 
+		get_template_part( 'page-templates/theme-sections/hero-banner', 'section' );
+		get_template_part( 'page-templates/theme-sections/top-categories', 'section' );
+		get_template_part( 'page-templates/theme-sections/latest-trending-articles', 'section' );
+		get_template_part( 'page-templates/theme-sections/explore-articles', 'section' );
+		get_template_part( 'page-templates/theme-sections/more-articles', 'section' );
+	?>
+</div>
+
+<?php get_footer();
